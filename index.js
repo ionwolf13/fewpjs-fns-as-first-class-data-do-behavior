24f72464980584330560bedc51bcfe297d4aa899
ionwolf13 <voli.scorp13@gmail.com>
2021-01-13 08:34:53 -0600

    commit tree bbb63bd332875647ab8d577b70486d43fd30a7d8

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time){
+  let newArr = []
+  newArr = time.split(':');
+  if (newArr[0] >= 0 && newArr[0] < 12)
+    return "Good Morning"
+
+  else if (newArr[0] >= 12 && newArr[0] <= 17)
+    return "Good Afternoon"
+
+  else
+  return "Good Evening"
+  end
+
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(string){
+   document.querySelector('#greeting').innerText = string
+}
